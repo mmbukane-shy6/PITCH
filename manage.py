4742f3e06c17a53923b1de73374adff7ead1187a
sheila egeidza <sheiladidy9@gmail.com>
2020-05-07 12:02:59 +0300

    installed email validation

--- manage.py
+++ manage.py
@@ -1,10 +1,12 @@
-# from app import create_app, db
+from app import create_app, db
 from flask_script import Manager, Server
 from app.models import *
 from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
-app = create_app('production')
+# app = create_app('test')
+app = create_app('development')
+# app = create_app('production')
 
 # Create manager instance
 manager = Manager(app)
