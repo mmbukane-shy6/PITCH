8fe044e81ca93bd88630698c2d1d63a825a33ee9
sheila egeidza <sheiladidy9@gmail.com>
2020-05-07 11:23:08 +0300

    installed email validator

--- manage.py
+++ manage.py
@@ -4,8 +4,6 @@ from app.models import *
 from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
-# app = create_app('test')
-# app = create_app('development')
 app = create_app('production')
 
 # Create manager instance
